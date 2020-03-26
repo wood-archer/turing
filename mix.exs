3b0ec6d89427591246610d4e5f9b71c5634e3262
Anil Wadghule <anildigital@gmail.com>
2020-03-26 17:47:21 +0530

    Add support for tests coverage report with excoveralls
    It adds following mix tasks
    
    MIX_ENV=test mix do coveralls.json
    MIX_ENV=test mix coveralls.html && open cover/excoveralls.html

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Turing.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -42,7 +49,8 @@ defmodule Turing.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:excoveralls, "~> 0.12.3"}
     ]
   end
 
