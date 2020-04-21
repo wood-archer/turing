bc107618c098b38c8e8bc8eb8e00f04826d3da83
Anil Wadghule <anildigital@gmail.com>
2020-04-22 00:06:04 +0530

    Integrate chat conversation screens with live chat

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -31,6 +31,10 @@ module.exports = (env, options) => ({
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+          test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          loader: 'file-loader',
       }
     ]
   },
---
+++ lib/turing_web/views/chat/conversation_view.ex
@@ -0,0 +1,3 @@
+defmodule TuringWeb.Chat.ConversationView do
+  use TuringWeb, :view
+end
