1fe3d903fa82d45cf5abe7e170c8c83999c44684
Anil Wadghule <anildigital@gmail.com>
2020-05-19 16:38:28 +0530

    Add Virtuoso config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -27,6 +27,15 @@ config :turing, TuringWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base
 
+use Mix.Config
+
+config :virtuoso,
+  wit_server_access_token: "VMA3ZOLFRPH3LGXZSQRPRKZGQORB3ICG",
+  watson_assistant_version: "",
+  watson_assistant_id: "",
+  watson_assistant_token: "",
+  default_nlp: Wit
+
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
